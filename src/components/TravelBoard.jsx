ea595b45267b45458e69a0d08c32fc2380724094
Aluno SENAI <Aluno@SN-363477.edu.fiesc.com.br>
2026-06-09 17:46:08 -0300

    terminar home.jsx

---
+++ src/components/TravelBoard.jsx
@@ -0,0 +1,10 @@
+import { colorsInstaTrip } from "../constants/colors";
+
+export default function TravelBoard(){
+
+
+    return (
+        <div className="h-100 w-200" style={{backgroundColor: colorsInstaTrip.card}}>
+        </div>
+    )
+}
--- src/tabs/Home.jsx
+++ src/tabs/Home.jsx
@@ -1,8 +1,16 @@
+import { useState } from 'react'
 import Logo from '../assets/logo_instatrip.png'
 import TravelPromptInput from '../components/TravelPromptInput'
 import { colorsInstaTrip } from '../constants/colors'
+import { AnimatePresence, motion } from 'motion/react'
+import TravelBoard from '../components/TravelBoard'
+
+const mainDivInitialStyles = 'flex flex-1 justify-center items-center'
+const mainDivStyles = 'flex flex-1 flex-row justify-between mx-45 items-center'
 
 export default function Home() {
+
+    const [clicado, setClicado] = useState(false)
 
 
     return (
@@ -12,11 +20,25 @@ export default function Home() {
                     <img src={Logo} className='scale-[0.5]'></img>
                 </div>
             </header>
-            <main className={`flex h-full`} style={{ backgroundColor: colorsInstaTrip.fundo }}>
-                <div className='flex flex-1 mt-20 justify-center items-center'>
-                    <TravelPromptInput></TravelPromptInput>
+
+            <main className={`flex flex-col flex-1`} style={{ backgroundColor: colorsInstaTrip.fundo }}>
+                <div className={!clicado ? mainDivInitialStyles : mainDivStyles}>
+                    <AnimatePresence>
+                        <motion.div layout key='travel-prompt' animate={{ scale: clicado ? 1.2 : 1 }} style={{transformOrigin: 'top center'}} transition={{ duration: 0.2 }}>
+                            <TravelPromptInput></TravelPromptInput>
+                        </motion.div>
+                        {clicado ?
+                            <motion.div layout key='travel-board' initial={{ opacity: 0, scale: 0 }} exit={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }}>
+                                <TravelBoard></TravelBoard>
+                            </motion.div>
+                            : null}
+                    </AnimatePresence>
+
                 </div>
             </main>
+
+
+            <button onClick={() => setClicado(!clicado)}>oi</button>
         </div>
     )
 }
